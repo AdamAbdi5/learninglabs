bff0f5cc303c128bc12b509dc9ea4b6851b6ffdf
Adam <adamabdi5@outlook.com>
2022-01-13 20:11:37 +0000

    add starting design

--- web/learninglabs/src/Pages/Home.jsx
+++ web/learninglabs/src/Pages/Home.jsx
@@ -1,10 +1,32 @@
-
+import "../App.sass"
 export default function Home(){
 
 
     return (
-        <div>
-            H1!
-        </div>
+        <>
+            <nav className="navbar is-dark">
+                <div navbar-brand>
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                        <button>Student</button>
+                        <button>Teacher</button>
+                    </a>
+                </div>
+                <div className="navbar-menu">
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <button className="button is-light is-outlined ">
+                                Student
+                            </button>
+                        </div>
+                        <div className="navbar-item">
+                            <button className="button is-dark is-inverted ">
+                                Teacher
+                            </button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 }
