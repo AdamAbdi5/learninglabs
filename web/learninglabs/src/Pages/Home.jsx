262460bb0b24b712815998ef1b33d2a765baf134
Adam Abdi <adamabdi5@outlook.com>
2022-01-22 13:23:49 +0000

    remove bulma

--- web/learninglabs/src/Pages/Home.jsx
+++ web/learninglabs/src/Pages/Home.jsx
@@ -4,29 +4,7 @@ export default function Home(){
 
     return (
         <>
-            <nav className="navbar is-dark">
-                <div navbar-brand>
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                        <button>Student</button>
-                        <button>Teacher</button>
-                    </a>
-                </div>
-                <div className="navbar-menu">
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <button className="button is-light is-outlined ">
-                                Student
-                            </button>
-                        </div>
-                        <div className="navbar-item">
-                            <button className="button is-dark is-inverted ">
-                                Teacher
-                            </button>
-                        </div>
-                        
-                    </div>
-                </div>
-            </nav>
+           
         </>
     );
 }
